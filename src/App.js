22018c391b84311cfb880ef9e776e9d7f126a61f
Mesbah <msbh214@gmail.com>
2021-01-27 14:36:09 +0600

    finished building image recog page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
+import Logo from "./Components/Logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+//import Particles from "react-particles-js";
+
+// const particleOptions = {
+//   particles: {
+//     number: {
+//       value: 80,
+//       density: {
+//         enable: true,
+//         value_area: 800,
+//       },
+//     },
+//   },
+// };
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        {/* <Logo />
+        {/* <Particles className="particles" params={particleOptions} /> */}
+        <div className="topBar">
+          <Logo />
+          <Navigation />
+        </div>
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css";
+import brain from "./icons8-brain-64.png";
+
+const Logo = () => {
+  return (
+    <div className="">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 45 }}
+        style={{ height: 130, width: 130 }}
+      >
+        <div className="Tilt-inner">
+          <img src={brain} alt="logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
